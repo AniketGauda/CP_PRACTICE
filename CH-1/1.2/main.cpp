8d219dd40a874399a35115f7d3f1943bfd8364d8
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-06 18:30:32 +0530

    Inputs in C++

---
+++ CH-1/1.2/main.cpp
@@ -0,0 +1,16 @@
+// Taking input from the user in C++.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // CP BOILER PLATE
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    // MAIN CODE
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "You entered Number is: " << num << "\n";
+}

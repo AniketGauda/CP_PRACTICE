9826437ab1b1f6162c2f61b615d7356f885124e0
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-07 16:32:30 +0530

    Basic C++ Programs

---
+++ CH-1/1.4/main.cpp
@@ -0,0 +1,43 @@
+// Problems
+
+// 1) Add 2 Numbers
+
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     // CP BOILERPLATE
+//     ios_base:: sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     // MAIN CODE
+//     int a,b;
+//     cin >> a >> b;
+//     int sum = a + b;
+//     cout << "Sum: " << sum << "\n";
+
+//     return 1;
+// }
+
+// 2) Area of Rectangle
+
+// int main() {
+//     int length, width;
+//     cin >> length >> width;
+//     int area = length * width;
+//     cout << "Area: " << area << "\n";
+
+//     return 1;
+// }
+
+// Simple Interest
+
+int main() {
+    double p, r, t;
+    cin >> p >> r >> t;
+    double simple_interest = (p * r * t) / 100;
+    cout << "Simple Interest: " << simple_interest << "\n";
+
+    return 1;
+}
+

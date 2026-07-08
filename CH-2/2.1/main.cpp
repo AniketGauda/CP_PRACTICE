2fc0b47600d02c6febfd45cf324990989742f2a7
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-08 12:43:46 +0530

    Fucntion Started

---
+++ CH-2/2.1/main.cpp
@@ -0,0 +1,11 @@
+// Function Introduction
+
+// Think function as some sort of vending machine. We will give some input to it. it will process 
+// & give the output.
+
+/*
+    Functions allows us to 
+    -- Reuse Logic (Write Once, Run Anywhere)
+    -- Breaking into modularity
+    -- Increase the Readability.
+*/

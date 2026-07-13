761b6153b33971c502174b2b9b709a58d19a150b
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-13 18:06:24 +0530

    Intro to DataTypes

---
+++ CH-3/3.1/main.cpp
@@ -0,0 +1,7 @@
+// In Chapter 3 we will cover data types.
+
+// In chapter 2 we learn that variable is nothing but a naming of a space in memory which can store data.
+// But data can be of different type so their storage also must have some distinction thus data types.
+
+// Computers store everything as bits, but C++ gives you types so you choose the right size and behavior.
+// Using wrong type → wrong answers (overflow) or wasted memory.

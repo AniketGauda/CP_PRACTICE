b1f2980c2e55cbfdb170ea61c160140949da9f16
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-14 15:17:26 +0530

    Strings

---
+++ CH-4/4.2/main.cpp
@@ -0,0 +1,17 @@
+// Strings - Sequence of Characters
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // CP BOILERPLATE
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // MAIN CODE
+    string s = "Hello World!";
+    cout << s << "\n";
+
+    return 1;
+}

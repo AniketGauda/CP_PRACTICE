906fa2b8366c69a90148f18b36bb96846e3369d4
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-21 17:23:27 +0530

    Operator Precedence

---
+++ CH-5/5.7/main.cpp
@@ -0,0 +1,38 @@
+// Operator Precedance & Associativity.
+
+/*
+    Level                           Operators
+    1 (highest)                     () ++ --
+    2                               * / %
+    3                               + -
+    4                               < > <= >=
+    5                               == !=
+    6                               &&
+    7                               `
+    8                               ?: (ternary)
+    9 (lowest)                      = += -= etc.
+*/
+
+/*
+
+    // Precedence
+
+    P = Paranthesis
+    U = Unary
+    M = Multiplication, Division, Modulo
+    A = Addition
+    S = Subtraction
+    R = Relational
+    E = Equality
+    B = Bitwise
+    L = Logical
+    T = Tertiary
+    A = Assignment
+    C = Comma
+
+    // Associativity
+    
+    Other than Assignment, Unary & Tertiary All are Left to Right.
+    Those 3 are only Right to Left.
+
+*/

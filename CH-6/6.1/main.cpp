1bd4c0a2e779e6da3cf0f4120b691bdc039c753b
Aniket Gauda <aniketgauda99@gmail.com>
2026-07-22 15:07:34 +0530

    Bitwise Operator

---
+++ CH-6/6.1/main.cpp
@@ -0,0 +1,24 @@
+// Bitwise Operator (Extremely Vital fo CP)
+
+/*
+    - Binary Number System Refresher.
+    - Computers Store integers in binary (base - 2) (0s & 1s).
+
+    - Decimal(13) = Binary(1101)
+    
+    Place values:  8  4  2  1
+    Bits:          1  1  0  1
+                   ↑     ↑
+                    8+4=12  +1 = 13
+
+*/
+
+
+/*
+
+    Each bit (binary digit) is one 0 or 1. In an 8-bit number:
+
+    Bit position (from right, 0-indexed):  7 6 5 4 3 2 1 0
+    Value of 13 as 8-bit:                  0 0 0 0 1 1 0 1
+
+*/
